53c654e1fe2dc907c9a3cef550afa3a62618e081
iamconnaught <wdonnelly22@gmail.com>
2019-05-11 15:17:11 -0500

    tested login register logout userIndex and homepage routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,80 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const session = require('express-session');
+const bcrypt = require('bcryptjs')
 
-router.post('.', async (req,res, next) => {
+// router.get('/', async ((req,res, next) => {
+	
+// }))
+
+
+router.post('/register', async (req,res, next) => {
+	const password = req.body.password;
+	const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+	const userDbEntry = {};
+	userDbEntry.username = req.body.username;
+	userDbEntry.password = passwordHash;
+
 	try {
-		const user = await User.create(req.body);
-		req.session.logged = true;
-		req.session.username = req.body.username;
+		const createdUser = await User.create(userDbEntry);	
+		req.session.loggedIn = true;
+		req.session.userDbId = createdUser._id;
+		console.log(createdUser);
 
 		res.json({
 			status: 200,
-			data: 'login successful'
+			data: createdUser
 		});
 
+		// res.redirect('/users/' + createdUser._id)
 	} catch (err){
 		next(err)
 	}
 })
+
+router.post('/login', async (req,res, next) => {
+	try {
+		const foundUser = await User.findOne({'username': req.body.username});
+		if(foundUser){
+			if(bcrypt.compareSync(req.body.password, foundUser.password)){
+				req.session.message = '';
+				req.session.loggedIn = true;
+				req.session.userDbId = foundUser._id;
+
+				res.json({
+					status: 200,
+					data: foundUser
+				})
+			}
+
+			else{
+				req.session.message = "Username or Password incorrect"
+			}
+		} else {
+			req.session.message = "Username or Password incorrect"
+		}
+	} catch (err){
+		next(err)
+	}
+})
+
+router.get('/logout', (req,res,next) => {
+	console.log('logout endpoint hit');
+	req.session.destroy((err) => {
+		if (err) {
+			next(err)
+		} else{
+			res.json({
+				status:200,
+				data: "logged out"
+			})
+		}
+	})
+	
+})
+
+
 
 module.exports = router;
---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const express        = require('express');
+const app            = express();
+const bodyParser     = require('body-parser');
+const cors           = require('cors');
+const session        = require('express-session');
+const bcrypt 		 = require('bcryptjs')
+const methodOverride = require('method-override')
+const superagent 	 = require('superagent')
+
+
+require('./db/db');
+
+//MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+const corsOptions = {
+	origin: `http://localhost:${process.env.PORT}`,
+	credentials: 'include',
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+const authController = require('./controllers/authController');
+app.use('/auth', authController);
+
+
+app.get('/', ((req,res, next) => {
+	try {
+		res.json('HOMEPAGE')
+	} catch (err){
+		next(err)
+	}
+}))
+
+const userController = require('./controllers/userController');
+app.use('/user', userController)
+
+app.listen(process.env.PORT, () => {
+	console.log('listening on port', process.env.PORT);
+})
+
+
+// 
